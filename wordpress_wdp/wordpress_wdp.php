ecb82a2c3156a2d459eb8ea019457c4a9073e4da
Wade Penistone <truemedia@mediacityonline.net>
2012-01-08 01:50:35 +0000

    arranged, renamed and added second dashboard widget functionality

--- wordpress_wdp/wordpress_wdp.php
+++ wordpress_wdp/wordpress_wdp.php
@@ -17,7 +17,8 @@ Author URI: http://mediacityonline.net
 /* include WP_CONTENT_DIR."/plugins/wordpress_wdp/theme.php"; */
 
 // ACTIONS/CONTROLLERS
-add_action('wp_dashboard_setup', 'demo_add_dashboard_widgets' ); /* Setup everything needed to view the widget correctly */
+add_action('wp_dashboard_setup', 'wdp_add_plugindev_dashboard_widgets' ); /* Setup everything needed to view the plugin dev widget correctly */
+add_action('wp_dashboard_setup', 'wdp_add_themedev_dashboard_widgets' ); /* Setup everything needed to view the theme dev widget correctly */
 // admin -> menu
 /* add_utility_page('Add a plugin', 'a custom title', 'administrator', 'add-plugin', 'development_view'); */
 add_shortcode( 'shortdebug', 'shortdebug_func' );
@@ -32,16 +33,16 @@ function theme_development_dashboard_view(){ /* Dashboard widget HTML inside con
 
 // MODELS the pure logical parts of the plugin
 /* prepares logic and pairs it with html output for plugin dev dashboard widget */
-function demo_dashboard_widget_function() {
+function wdp_dashboard_plugindev_widget_function() {
 
 /* view for plugin dev dash widget */
 echo(plugin_development_dashboard_view());
 }
 /* prepares logic and pairs it with html output for theme dev dashboard widget */
-function demo_dashboard_theme_widget_function() {
+function wdp_dashboard_themedev_widget_function() {
 
 /* view for theme dev dash widget */
-echo(plugin_development_dashboard_view());
+echo(theme_development_dashboard_view());
 }
 // [shortcode var_pageurl="wordpress function" var_user="wordpress function"]
 function shortdebug_func( $atts ) {
@@ -54,9 +55,9 @@ function shortdebug_func( $atts ) {
 }
 
 // INSTALLERS
-// install a dashboard item
-function demo_add_dashboard_widgets() {
-wp_add_dashboard_widget('demo_dashboard_widget', 'WordPress create a plugin', 'demo_dashboard_widget_function');
+/* install the plugin dev dashboard item */
+function wdp_add_plugindev_dashboard_widgets() {
+wp_add_dashboard_widget('wdp_plugindev_dashboard_widget', 'WordPress create a plugin', 'wdp_dashboard_plugindev_widget_function');
 
 // Globalize the metaboxes array, this holds all the widgets for wp-admin
 global $wp_meta_boxes;
@@ -66,9 +67,32 @@ global $wp_meta_boxes;
 $normal_dashboard = $wp_meta_boxes['dashboard']['normal']['core'];
 
 // Backup and delete our new dashbaord widget from the end of the array
-$demo_widget_backup = array('demo_dashboard_widget' =>
-$normal_dashboard['demo_dashboard_widget']);
-unset($normal_dashboard['demo_dashboard_widget']);
+$demo_widget_backup = array('wdp_plugindev_dashboard_widget' =>
+$normal_dashboard['wdp_plugindev_dashboard_widget']);
+unset($normal_dashboard['wdp_plugindev_dashboard_widget']);
+
+// Merge the two arrays together so our widget is at the beginning
+$sorted_dashboard = array_merge($demo_widget_backup, $normal_dashboard);
+
+// Save the sorted array back into the original metaboxes
+$wp_meta_boxes['dashboard']['normal']['core'] = $sorted_dashboard;
+}
+
+/* install the theme dev dashboard item */
+function wdp_add_themedev_dashboard_widgets() {
+wp_add_dashboard_widget('wdp_themedev_dashboard_widget', 'WordPress create a theme', 'wdp_dashboard_themedev_widget_function');
+
+// Globalize the metaboxes array, this holds all the widgets for wp-admin
+global $wp_meta_boxes;
+
+// Get the regular dashboard widgets array
+// (which has our new widget already but at the end)
+$normal_dashboard = $wp_meta_boxes['dashboard']['normal']['core'];
+
+// Backup and delete our new dashbaord widget from the end of the array
+$demo_widget_backup = array('wdp_themedev_dashboard_widget' =>
+$normal_dashboard['wdp_themedev_dashboard_widget']);
+unset($normal_dashboard['wdp_themedev_dashboard_widget']);
 
 // Merge the two arrays together so our widget is at the beginning
 $sorted_dashboard = array_merge($demo_widget_backup, $normal_dashboard);
